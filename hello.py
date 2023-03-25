cecd6446db63c4a304ed7ee57582a46f994b3366
aviiciii <laavesh1@gmail.com>
2023-03-25 19:16:59 +0530

    created ui
    added labels, input fields and button

--- hello.py
+++ hello.py
@@ -1,10 +1,37 @@
 import kivy 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+class MyGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGridLayout, self).__init__(**kwargs)
+
+        self.cols = 2
+
+        self.add_widget(Label(text="Long Url: ", font_size=36))
+        self.long_url = TextInput(multiline=False)
+        self.add_widget(self.long_url)
+
+        self.add_widget(Label(text="Short Url: ", font_size=46))
+        self.short_url = TextInput(multiline=False)
+        self.add_widget(self.short_url)
+
+        self.shorten_button = Button(text="Shorten", font_size=40)
+        self.shorten_button.bind(on_press=self.shorten)
+        self.add_widget(self.shorten_button)
+
+    def shorten(self, instance):
+        print("Button pressed!!")
+        long_url = self.long_url.text
+        short_url = self.short_url.text
 
 class HelloApp(App):
     def build(self):
-        return Label(text='Hello World', font_size=100)
+        return MyGridLayout()
     
 if __name__ == '__main__':
     HelloApp().run()
