235772dda5e4aeb438b4545f48bb09d9fe8e5ed1
aviiciii <laavesh1@gmail.com>
2023-03-25 22:13:29 +0530

    changed grid layout

--- hello.py
+++ hello.py
@@ -6,37 +6,78 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
+
+# API 
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+import json
+
 class MyGridLayout(GridLayout):
     def __init__(self, **kwargs):
         super(MyGridLayout, self).__init__(**kwargs)
 
-        self.cols = 2
+        self.cols = 1
 
-        self.add_widget(Label(text="Long Url: ", font_size=36))
+        self.input_form = GridLayout()
+
+        self.input_form.cols = 2
+
+
+
+        self.input_form.add_widget(Label(text="Long Url: ", font_size=36))
         self.long_url = TextInput(multiline=False)
-        self.add_widget(self.long_url)
+        self.input_form.add_widget(self.long_url)
 
-        self.add_widget(Label(text="Short Url: ", font_size=46))
+        self.input_form.add_widget(Label(text="Short Url: ", font_size=46))
         self.slug = TextInput(multiline=False)
-        self.add_widget(self.slug)
+        self.input_form.add_widget(self.slug)
+
+        self.add_widget(self.input_form)
 
         self.shorten_button = Button(text="Shorten", font_size=40)
         self.shorten_button.bind(on_press=self.shorten)
         self.add_widget(self.shorten_button)
+
+        self.message = Label(text="", font_size=40)
+        self.add_widget(self.message)
 
     def shorten(self, instance):
         print("Button pressed!!")
         long_url = self.long_url.text
         slug = self.slug.text
 
+        print(long_url, type(long_url))
+        print(slug, type(slug))
 
-        
+
+        if slug:
+            print("Slug is not empty")
+            pass
+        else:
+            print("Slug is empty")
+
+            # API call 
+            url = "https://api.short.io/links/lnk_2MIx_9dCPkouo4fY"
+            payload = json.dumps({"allowDuplicates": False, "domain": "we.laavesh.ml", "originalURL": long_url })
+            headers = {
+                'accept': "application/json",
+                'content-type': "application/json",
+                'authorization': os.getenv("API_KEY")
+            }
+
+            response = requests.request("POST", url, data=payload, headers=headers)
+
+            res = json.dumps(response.json(), indent=4) 
+            print(res)
 
 
 
-        short_url = "https://link.laavesh.ml/" + slug
 
-        self.add_widget(Label(text=f"Shortened: {short_url}", font_size=20))
+        short_url = "https://we.laavesh.ml/" + slug
+
+        self.add_widget(Label(text=f"Shortened!", font_size=20))
 
 class HelloApp(App):
     def build(self):
